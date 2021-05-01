85741664e291ba7328874c2201c7578e23ffcaed
Abdulsametileri <sametileri07@gmail.com>
2021-05-01 18:09:29 +0300

    Added nprogress to provide browser responsiveness with the heavy data load

--- components/Layout.js
+++ components/Layout.js
@@ -4,11 +4,27 @@ import Container from "react-bootstrap/Container";
 import styles from '../styles/Layout.module.scss'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import Head from 'next/head'
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const LayoutComponent = ({ children }) => {
   return (
     <>
-      <Meta />
+      <Head>
+        <Meta />
+        <link rel='icon' href='/favicon.ico' />
+        <title>Interview Preperation</title>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+
       <Nav />
       <ToastContainer />
       <main>
