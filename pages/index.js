9a999afc08a482b5085ef4915e354851d1ec9144
Abdulsametileri <sametileri07@gmail.com>
2021-05-01 22:13:25 +0300

    Added descending order functionality for code snippet list

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,19 @@ import indexStyles from '@/styles/Index.module.scss'
 import DebounceSearch from "@/components/DebounceSearch";
 import SnippetList from "@/components/SnippetList";
 import Spacer from "@/components/Spacer";
+import {SearchInCodeSnippetList} from "@/api/codeSnippet";
 
 const Index = ({snippets}) => {
+  const search = async (keyword) => {
+    await SearchInCodeSnippetList(keyword)
+  }
+
   return (
     <>
       <div className={indexStyles.searchContainer}>
         <DebounceSearch
           className={indexStyles.debounceInput}
-          onChange={event => console.log(event.target.value)}
+          onChange={event => search(event.target.value)}
         />
       </div>
 
@@ -31,11 +36,14 @@ export async function getServerSideProps() {
   await dbConnect()
 
   const result = await Code.find({})
+    .sort({"_id":-1}) // -1 mean descending order
+
   const snippets = result.map((doc) => {
     const snippet = doc.toObject()
     snippet._id = snippet._id.toString()
     return snippet
   })
+
   return {props: {snippets}}
 }
 
