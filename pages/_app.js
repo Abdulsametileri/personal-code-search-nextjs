c65ce2665046ad3f608136060c8d4aa81fe27bd0
Abdulsametileri <sametileri07@gmail.com>
2021-05-01 19:38:15 +0300

    Added dummy commit to trigger deployment again.

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,9 @@ import Layout from "@/components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-      <>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     )
 }
 
